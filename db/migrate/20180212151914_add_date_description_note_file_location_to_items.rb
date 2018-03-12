ea46b07f161717ff5342efef6e7cfae19a60ef81
HazelPan <sea39801991@gmail.com>
2018-03-12 15:38:50 +0800

    Delete 3 migrate files

--- db/migrate/20180212151914_add_date_description_note_file_location_to_items.rb
+++
@@ -1,8 +0,0 @@
-class AddDateDescriptionNoteFileLocationToItems < ActiveRecord::Migration[5.1]
-  def change
-    add_column :items, :due_date, :date
-    add_column :items, :description, :text
-    add_column :items, :note, :text
-    add_column :items, :file_location, :string
-  end
-end
--- db/migrate/20180217122142_add_mark_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddMarkToItems < ActiveRecord::Migration[5.1]
-  def change
-    add_column :items, :mark, :symbol
-  end
-end
